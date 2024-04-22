b3f6f500f8a2cfb7a5d062a179d8ee09c79d371c
Karol Kostrzewa <karol.kostrzewa@snowflake.com>
2024-04-22 15:13:24 +0200

    set init order

--- javascripts/discourse/initializers/discourse-custom-header-links.js
+++ javascripts/discourse/initializers/discourse-custom-header-links.js
@@ -1,8 +1,14 @@
-import { apiInitializer } from "discourse/lib/api";
+import { withPluginApi } from "discourse/lib/plugin-api";
 import CustomHeaderLinks from "../components/custom-header-links";
 
-export default apiInitializer("1.14.0", (api) => {
-  api.renderInOutlet("before-header-panel", CustomHeaderLinks);
-  api.replaceIcon('bars', 'sidebar-icon');
-  api.replaceIcon('mdf-menu', 'sidebar-icon');
-});
+export default {
+    name: "Custom Header Links",
+    after: "discourse-material-design-icons-filled",
+    initialize() {
+        withPluginApi("1.14.0", api => {
+            api.renderInOutlet("before-header-panel", CustomHeaderLinks);
+            api.replaceIcon('bars', 'sidebar-icon');
+            api.replaceIcon('mdf-menu', 'sidebar-icon');
+        });
+    }
+};
