c9c728b5076b1635d2321d40f07fd73293eb6e54
Karol Kostrzewa <karol.kostrzewa@snowflake.com>
2024-04-24 09:26:17 +0200

    rm hamburger menu override

--- javascripts/discourse/initializers/discourse-custom-header-links.js
+++ javascripts/discourse/initializers/discourse-custom-header-links.js
@@ -3,12 +3,9 @@ import CustomHeaderLinks from "../components/custom-header-links";
 
 export default {
     name: "custom-header-links",
-    after: "material-design-icons-filled",
     initialize() {
         withPluginApi("1.14.0", api => {
             api.renderInOutlet("before-header-panel", CustomHeaderLinks);
-            api.replaceIcon('bars', 'sidebar-icon');
-            api.replaceIcon('mdf-menu', 'sidebar-icon');
         });
     }
 };
