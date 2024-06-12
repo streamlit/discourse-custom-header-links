788059b549e843b1e8a6fa192a1c0caee5610b81
Michał Gołębiowski-Owczarek <m.golebiowski-owczar@snowflake.com>
2024-06-12 14:40:00 +0200

    SNOW-1423522 Apply Prettier on the JS, update .gitignore, tweak header link spacing (#4)
    * SNOW-1423522 Apply Prettier on the JS, update .gitignore
    
    * SNOW-1423522 Tweak header link spacing

--- javascripts/discourse/initializers/discourse-custom-header-links.js
+++ javascripts/discourse/initializers/discourse-custom-header-links.js
@@ -2,10 +2,10 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 import CustomHeaderLinks from "../components/custom-header-links";
 
 export default {
-    name: "custom-header-links",
-    initialize() {
-        withPluginApi("1.14.0", api => {
-            api.renderInOutlet("before-header-panel", CustomHeaderLinks);
-        });
-    }
+  name: "custom-header-links",
+  initialize() {
+    withPluginApi("1.14.0", (api) => {
+      api.renderInOutlet("before-header-panel", CustomHeaderLinks);
+    });
+  },
 };
